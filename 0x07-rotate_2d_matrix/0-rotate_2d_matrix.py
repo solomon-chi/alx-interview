ff301021a4bffca271fe0d35ab6b8b282fe96c97
solomon-chi <111745098+solomon-chi@users.noreply.github.com>
2023-09-14 23:41:48 +0100

    Add files via upload

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Rotating a 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Given an n x n 2D matrix, rotate it 90 degrees clockwise
+    """
+    n = len(matrix)
+
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Reverse each row
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+
+    rotate_2d_matrix(matrix)
+    print(matrix)
---
+++ 0x07-rotate_2d_matrix/README (1).md
@@ -0,0 +1,13 @@
+# 0x07. Rotate 2D Matrix
+```Algorithm``` | ```Python```
+## Requiremnts
+### General
+* Allowed editors: vi, vim, emacs
+* All the files will be interpreted/compiled on Ubuntu 20.04 LTS using python3 (version 3.8.10)
+* All the files should end with a new line
+* The first line of all your files should be exactly #!/usr/bin/python3
+* A README.md file, at the root of the folder of the project, is mandatory
+* Code should use the pycodestyle style (version 2.8.0)
+* Not allowed to import any module
+* All modules and functions must be documented
+* All the files must be executable
